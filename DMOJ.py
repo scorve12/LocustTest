d8ec2ab2e5a729d117d4dcc5877b1f71c61041da
scorve12 <scorve5322@gmail.com>
2024-09-24 17:46:55 +0900

    feat submision task 추가

--- DMOJ.py
+++ DMOJ.py
@@ -1,37 +1,60 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, task, between
+import random
 
-class UserBehavior(TaskSet):
+class UserBehavior(HttpUser):
+    wait_time = between(1, 2)
+
+    def get_csrf(self):
+        response = self.client.get("/accounts/login")
+        self.csrftoken = response.cookies['csrftoken']
+        self.headers = {'X-CSRFToken': self.csrftoken, "Content-Type": "application/x-www-form-urlencoded"}
+
     @task(1)
     def login(self):
-        # 로그인 요청을 위한 데이터
-        payload = {
-            'csrfmiddlewaretoken': 'Sr6vmRxSweem7P8bN2wbfTVrjt0wSZAOxD9PK5ZPRZDFaGTj4dzkLoVr7Z3qa7KB',
-            'username': 'test1',
-            'password': 'dmoj1234',
-            'next': '/'
+
+        user_id = random.randint(1, 200)
+        username = f"test{user_id}"
+
+        self.get_csrf()
+
+        user_data = {
+            "username": username,
+            "password": "dmoj1234",
+            "csrfmiddlewaretoken": self.csrftoken
         }
-        
-        # POST 요청 보내기
-        self.client.post(
-            "/accounts/login/?next=/",
-            data=payload,
-            headers={
-                "Host": "uncertain.today", #호스트정보
-                "Connection": "keep-alive", #연결유지
-                "Content-Type": "application/x-www-form-urlencoded",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Referer": "http://uncertain.today/accounts/login/?next=/",
-                "Accept-Encoding": "gzip, deflate",
-                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                "Cookie": "csrftoken=6aOVkudrl4GAuywR9VJzOGETleHtMyEGLmRfIIFoGP5TxphZq6MIkbET9KKn4GOt"
-            }
-        )
+        # print("Current cookies in Login:" + str(self.client.cookies))
 
-    @task(2)
-    def load_homepage(self):
-        self.client.get("/")  # 홈페이지 요청
+        response = self.client.post("/accounts/login/",
+                                data=user_data,
+                                headers=self.headers)
 
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    wait_time = between(1, 5)  # 요청 사이의 대기 시간
+        if response.status_code == 200:
+            print("로그인 성공!")
+            print(response.cookies)
+            print(response.text)
+        else:
+            print("로그인 실패!")
+            print(f"상태 코드: {response.status_code}, 응답: {response.text}")
+
+        cookies = self.client.cookies.get_dict()
+        self.get_csrf()
+        self.headers['sessionid'] = cookies.get('sessionid')
+
+        pro_data = {
+            'source': "print('test')",
+            'language': 8,
+            'judge': '',
+            'csrfmiddlewaretoken': self.csrftoken
+        }
+
+        response = self.client.post("/problem/test1/submit",
+                                data=pro_data,
+                                headers=self.headers)
+
+        if response.status_code == 200:
+            print("제출 성공!")
+            print(response.cookies)
+            print(response.text)
+        else:
+            print("제출 실패!")
+            print(f"상태 코드: {response.status_code}, 응답: {response.text}")
--- Quing.py
+++ Quing.py
@@ -1,38 +1,31 @@
-from locust import HttpLocust, TaskSet, task, HttpUser, between
-import json
-
+from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)  # 사용자 간 대기 시간
+    wait_time = between(1, 2)
 
-    
     def on_start(self):
-        # 로그인 과정을 수행하며, CSRF 토큰과 sessionid를 추출합니다. 
-        # CSRF 토큰을 가져오기 위해 /api/profile에 GET 요청을 보냅니다.
-        with self.client.get("/api/profile", catch_response=True) as response:
-            cookies = response.cookies
-            csrftoken = cookies.get('csrftoken', None)
-            print("CSRFTOKEN:", csrftoken)
-        
-        # 추출된 CSRF 토큰을 사용하여 로그인을 시도합니다.
-        response = self.client.post("/api/login", json={
+        self.client.get("/api/profile")
+        self.csrftoken = self.client.cookies.get('csrftoken')
+        print("CSRFTOKEN:", self.csrftoken)
+
+        with self.client.post("/api/login", json={
             "username": "test1",
             "password": "cnlab01"
         }, headers={
-            "X-CSRFToken": csrftoken
-        })
+            "X-CSRFToken": self.csrftoken
+        }, catch_response=True) as response:
+            if response.cookies:
+                self.sessionid = response.cookies.get('sessionid')
+                print("SESSIONID:", self.sessionid)
 
-        # 응답에서 sessionid를 추출하여 저장합니다.
-        if response.cookies:
-            self.sessionid = response.cookies.get('sessionid', None)
-            print("Extracted SESSIONID:", self.sessionid)
-    
     @task
     def profile(self):
-        response = self.client.get("/api/profile", cookies={'sessionid': self.sessionid})
-        print(response.text)
-        
-            
-        
-        
-      
+        with self.client.post("/api/submission", json={
+            "problem_id": 1,
+            "language": "Python3",
+            "code": "print(\"hello\")"
+        }, headers={
+            'Cookie': f'sessionid={self.sessionid}; csrftoken={self.csrftoken}',
+            'X-CSRFToken': self.csrftoken
+        }, catch_response=True) as response:
+            print(response.text)
